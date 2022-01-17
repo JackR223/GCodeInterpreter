85bb8c623176d86ebfb3f7e8bf2d00e86a2f2bce
JackR223 <sgjrive@liverpool.ac.uk>
2022-01-17 18:14:50 +0000

    Added aditional file to help with DRY for G0 & G1

--- Commands/G1LinearInterpol.py
+++ Commands/G1LinearInterpol.py
@@ -1,17 +1,12 @@
-import math
+import StraightLineInterpol
 
 def interpolate(cmdIn, currPosition):
 
     values = cmdIn.split()
-    points = []
-
-    startpointX = currPosition[0]
-    startpointY = currPosition[1]
-    startpointZ = currPosition[2]
 
     endpointX = currPosition[0]
-    endPointY = currPosition[1]
-    endPointZ = currPosition[2]
+    endpointY = currPosition[1]
+    endpointZ = currPosition[2]
 
     #pass values into variables
     for value in values:
@@ -22,18 +17,6 @@ def interpolate(cmdIn, currPosition):
         elif value.startswith("Z"):
             endPointZ = float(value[1:len(value)])
 
-
-    n = 10 #number of points on line to interpolate
-
-    points = []
-    for i in range(n+1):
-        a = float(i) / n    #rescale 0 < i < n --> 0 < a < 1
-        x = (1 - a) * startpointX + a * endpointX    #interpolate x coordinate
-        y = (1 - a) * startpointY + a * endPointY    #interpolate y coordinate
-        z = (1 - a) * startpointZ + a * endPointZ    #interpolate z coordinate
-        #points1.append((round(x),round(y),round(z)))
-        
-        #array to store list of points to pass through 
-        points.append((x, y, z))
+    points = StraightLineInterpol.interpolate(currPosition, endpointX, endPointY, endPointZ)
 
     return points
